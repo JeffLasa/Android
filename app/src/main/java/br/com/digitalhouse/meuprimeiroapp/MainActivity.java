14a2922f836b7e20b9dd7fabebb4f1e0c755e9b3
JeffLasa <jeff.Lasa@hotmail.com>
2019-05-24 12:08:47 -0300

    first commit

---
+++ app/src/main/java/br/com/digitalhouse/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,35 @@
+package br.com.digitalhouse.meuprimeiroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void botaoApertado(View view) {
+
+        EditText respostaAltura = findViewById(R.id.resposta_altura_id);
+
+        EditText respostaPeso = findViewById(R.id.texto_resposta__peso_id);
+
+        Float altura = Float.parseFloat(respostaAltura.getText().toString());
+        Float peso = Float.parseFloat(respostaPeso.getText().toString());
+        Float calculo = peso/(altura*altura);
+        TextView resposta = findViewById(R.id.resposta_calculo_imc_id);
+        resposta.setText("O seu IMC é "+calculo);
+
+        
+
+
+    }
+
+}
