3289866f0c7809fbd3fc0aecef84ab83c6561ac9
jeffLasa <jeff.lasa@hotmail>
2019-05-27 12:23:21 -0300

    Android LinearLayout

--- app/src/main/java/br/com/digitalhouse/meuprimeiroapp/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/meuprimeiroapp/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.digitalhouse.meuprimeiroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         TextView resposta = findViewById(R.id.resposta_calculo_imc_id);
         resposta.setText("O seu IMC é "+calculo);
 
+        Log.i(MainActivity.class.toString(),"O botão foiapertado");
+
+        Toast.makeText(this,"eu",INPUT_METHOD_SERVICE);
         
 
 
